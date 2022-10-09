755289bc6edc7877963fea90b5b18a389bf24fc3
ajea <smithajea@rocketmail.com>
2022-10-09 15:27:11 -0400

    - convert meter to foot and vise-versa

--- Challenge - Unit Conversion/ContentView.swift
+++ Challenge - Unit Conversion/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var selectedInput = "meter"
+    @State private var selectedOutput = "foot"
+    @State private var unit: Double = 1
+    let inputUnits = ["meter", "foot"]
+    let outputUnits = ["meter", "foot"]
+    
+    var convertUnits: Double {
+        if(selectedInput == "meter" && selectedOutput == "foot"){
+            return unit * 3.281
+        }else if (selectedInput == "foot" && selectedOutput == "meter"){
+            return unit / 3.281
         }
-        .padding()
+        return 0
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter unit", value: $unit, format: .number)
+                        .keyboardType(.numberPad)
+                    Picker("Inputs", selection: $selectedInput) {
+                        ForEach(inputUnits, id: \.self) { units in
+                            Text(units)
+                        }
+                    }.pickerStyle(.segmented)
+                }header: {
+                    Text("Convert From:")
+                }
+                Section {
+                    Picker("Outputs", selection: $selectedOutput) {
+                        ForEach(outputUnits, id: \.self) { units in
+                            Text(units)
+                        }
+                    }.pickerStyle(.segmented)
+                }header: {
+                    Text("Convert To:")
+                }
+                Section {
+                    Text("\(convertUnits)")
+                }header: {
+                    Text("Results")
+                }
+            }
+            .navigationTitle("Length Conversion")
+        }
     }
 }
 
--- Challenge - Unit Conversion/Unit_ConversionApp.swift
+++ Challenge - Unit Conversion/Unit_ConversionApp.swift
@@ -3,7 +3,7 @@
 import SwiftUI
 
 @main
-struct ___PACKAGENAME:identifier___App: App {
+struct Unit_Conversion: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
